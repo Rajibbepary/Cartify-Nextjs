95a963828f9e25ea6170970a744badb9dee468ea
Rajibbepary <rajib635356@gmail.com>
2026-09-04 21:56:46 +0600

    clerk setup

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Outfit } from "next/font/google";
 import "./globals.css";
 import { AppContextProvider } from "@/context/AppContext";
 import { Toaster } from "react-hot-toast";
-
+import { ClerkProvider } from '@clerk/nextjs'
 const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
 
 export const metadata = {
@@ -13,12 +13,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
       <html lang="en">
+        <ClerkProvider>
         <body className={`${outfit.className} antialiased text-gray-700`} >
           <Toaster />
           <AppContextProvider>
             {children}
           </AppContextProvider>
         </body>
+        </ClerkProvider>
       </html>
   );
 }
